2212d32b01ae8df38cc71ea94045c45b763ecbc6
aatiii <helen_ati@163.com>
2016-02-12 16:25:45 -0500

    new pages added

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,12 +1,60 @@
 "use strict";
 
 // setup your IIFE (Immediately Invoked Function Expression)
-(function(){
-   
-  console.log("App Started..."); 
+(function () {
+
+
+
+
+    console.log("Hailun Li's Portfolio");
     
-   var firstParagraph;
+
     
-   firstParagraph = document.getElementById("firstParagraph");
-   firstParagraph.innerHTML = "My New Paragraph Data";
+    // declare an anoymous function with named alias
+    var replaceFirstParagraph = function() {
+        console.log("inside replaceFirstParagraph function");
+        
+        var firstParagraph;
+
+        firstParagraph = document.getElementById("firstParagraph");
+        firstParagraph.innerHTML = "blah blah blah";
+    }
+    
+        // text on bio page
+    var replaceBio = function() {
+        console.log("inside replaceBio function");
+        
+        var bio;
+
+        bio = document.getElementById("bio");
+        bio.innerHTML = "about me blah blah blah";
+    }
+    
+    // call the function
+    replaceBio();    
+    
+    // call the replaceFirstParagraph function
+    replaceFirstParagraph();
+    
+    
 })();
+
+
+(function () {
+   
+    // text on bio page
+    var replaceBio = function() {
+        console.log("inside replaceBio function");
+        
+        var bio;
+
+        bio = document.getElementById("bio");
+        bio.innerHTML = "about me blah blah blah";
+    }
+    
+    // call the function
+    replaceBio();    
+    
+})();
+
+
